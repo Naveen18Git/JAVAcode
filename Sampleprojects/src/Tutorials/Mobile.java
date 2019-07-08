b02f55089230e0fec4afd6d5a28c1f914c788b2f
naveen <naveen@192.168.0.4>
2019-07-08 12:20:53 +0530

    First commit

---
+++ Sampleprojects/src/Tutorials/Mobile.java
@@ -0,0 +1,12 @@
+package Tutorials;
+
+public class Mobile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("I have REDMI note 5 PRO");
+	}
+	
+
+}
